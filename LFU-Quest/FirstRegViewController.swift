26a3c7e2ccd12517415f8421b999372df6b4023d
ExEr7um <exer7um@gmail.com>
2018-04-09 16:20:19 +0300

    Registration Data Collection
    • Added userName, userSurname, userEmail and userPassword variables
    • Added actions to buttons which collect this data
    • Added password checker on 2nd registration screen
    • Added error message on 2nd registration screen if passwords didn't match

--- LFU-Quest/FirstRegViewController.swift
+++ LFU-Quest/FirstRegViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 import Hero
+import Spring
 
 class FirstRegViewController: UIViewController {
 
+    @IBOutlet weak var nameTextFieldView: SpringTextField!
+    @IBOutlet weak var surnameTextFieldView: SpringTextField!
+    @IBOutlet weak var emailTextFieldView: SpringTextField!
+    @IBAction func nextButtonView(_ sender: SpringButton) {
+        var userName: String = nameTextFieldView.text!
+        var userSurname: String = surnameTextFieldView.text!
+        var userEmail: String = emailTextFieldView.text!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
 }
--- LFU-Quest/SecondRegViewController.swift
+++ LFU-Quest/SecondRegViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 import Hero
 
+
 class SecondRegViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextFieldView: UITextField!
+    @IBOutlet weak var repeatPasswordTextFieldView: UITextField!
+    @IBAction func registerButtonView(_ sender: Any) {
+        var userPassword: String = passwordTextFieldView.text!
+        var repeatPassword: String = repeatPasswordTextFieldView.text!
+        if userPassword == repeatPassword {
+            
+        }
+        else {
+            let alert = UIAlertController(title: "Пароли не совпадают!", message: "Проверьте правильность написания паролей", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
 }
